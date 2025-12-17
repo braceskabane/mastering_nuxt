0a5910ad44d5269cc543dae368338ae4a9785568
braceskabane <allzoldyck31@gmail.com>
2025-12-17 16:08:49 +0700

    feat: implement dynamic routes with optional parameters

--- pages/course/[chapterSlug]/[[lessonSlug]].vue
+++ pages/course/[chapterSlug]/[[lessonSlug]].vue
@@ -2,10 +2,11 @@
   <div>
     <h2 class="font-bold">Lesson</h2>
     <p>This is a lesson</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
   </div>
 </template>
 
 <script setup>
 
 </script>
-
