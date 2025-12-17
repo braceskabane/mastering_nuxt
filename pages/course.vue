ca877b1d22d5fa925e286900123b5c668ec3f17c
braceskabane <allzoldyck31@gmail.com>
2025-12-17 15:58:11 +0700

    feat: implement nested routes for course pages

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
     <div class="prose mb-12">
-      <h1 class="text-4xl">
+      <h1>
         <span class="font-medium">Course: </span>
         <span class="font-bold">Mastering Nuxt</span>
       </h1>
@@ -9,12 +9,12 @@
 
     <div class="flex flex-row justify-center flex-grow">
       <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
-        <h3>Chapters</h3>
+        <h3 class="font-bold">Chapters</h3>
+        
       </div>
 
       <div class="prose p-12 bg-red-100 rounded-md w-[65ch]">
-        <h2>Lesson</h2>
-        <p>This is a lesson</p>
+        <NuxtPage />
       </div>
     </div>
   </div>
