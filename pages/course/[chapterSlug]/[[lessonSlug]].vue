4eb7639efbf67c04512ed52693adc3eafd037150
braceskabane <allzoldyck31@gmail.com>
2025-12-17 16:30:18 +0700

    upgrade ui data styling

--- pages/course/[chapterSlug]/[[lessonSlug]].vue
+++ pages/course/[chapterSlug]/[[lessonSlug]].vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    <h2 class="font-bold">Lesson</h2>
-    <p>This is a lesson</p>
-    <p>{{ chapter.title }}</p>
-    <p>{{ lesson.title }}</p>
+  <div v-if="lesson">
+    <p class="text-sm font-medium text-gray-500 uppercase mb-2">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    
+    <h2 class="text-3xl font-bold mb-4">{{ lesson.title }}</h2>
+    
+    <a 
+      v-if="lesson.downloadUrl"
+      :href="lesson.downloadUrl" 
+      target="_blank"
+      class="text-blue-600 hover:underline text-sm mb-6 inline-block"
+    >
+      Download Video
+    </a>
+    
+    <div class="prose max-w-none">
+      <p class="whitespace-pre-line">{{ lesson.text }}</p>
+    </div>
+  </div>
+  <div v-else>
+    <p>Lesson not found</p>
   </div>
 </template>
 
@@ -18,6 +35,7 @@ const chapter = computed(() => {
 });
 
 const lesson = computed(() => {
+  if (!chapter.value || !route.params.lessonSlug) return null;
   return chapter.value.lessons.find(
     (lesson) => lesson.slug === route.params.lessonSlug
   );
