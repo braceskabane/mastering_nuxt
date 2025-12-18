1e348b8efe9cb90a21278c1c505de3ff3d217084
braceskabane <allzoldyck31@gmail.com>
2025-12-18 15:24:40 +0700

    Add vueuse integration and fix Tailwind CSS compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
   css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
 })
